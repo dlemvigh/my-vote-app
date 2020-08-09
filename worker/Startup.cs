d75fa09c8a2ba304524304632242c513a9a0407b
David Lemvigh <dlemvigh@gmail.com>
2020-08-10 00:39:58 +0200

    Dotnet core watch

--- worker/Startup.cs
+++ worker/Startup.cs
@@ -32,7 +32,7 @@ namespace worker
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!!");
+                    await context.Response.WriteAsync("Hello World!22!");
                 });
             });
         }
